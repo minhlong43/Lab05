b353e003c031d395afed773bbf43116609cf0419
DoMinhLong <dominhlong43@gmail.com>
2022-05-05 23:31:58 +0700

    Functional Components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Main from "./components/MainComponent";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
